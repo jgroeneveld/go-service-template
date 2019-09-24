0cdad5ba138e9ba81c38b825090387c3b59cde9f
Jaap Groeneveld <moin@jgroeneveld.de>
2019-09-24 16:51:50 +0200

    move test helper into seperate file

--- app/integration_test.go
+++ app/integration_test.go
@@ -3,10 +3,7 @@ package app
 import (
 	"github.com/jgroeneveld/schema"
 	"github.com/jgroeneveld/trial/assert"
-	"github.com/jgroeneveld/trial/th"
-	"io"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 )
 
@@ -19,35 +16,4 @@ func TestRootResponse(t *testing.T) {
 	AssertJSONSchema(t, response.Body, schema.Map{
 		"Foo": "Hello World",
 	})
-}
-
-func Get(server http.Handler, url string) *httptest.ResponseRecorder {
-	return SendRequest(server, "GET", url, nil)
-}
-
-func SendRequest(server http.Handler, method, url string, body io.Reader) *httptest.ResponseRecorder {
-	req, err := http.NewRequest(method, url, body)
-	if err != nil {
-		panic(err)
-	}
-
-	recorder := httptest.NewRecorder()
-	server.ServeHTTP(recorder, req)
-
-	return recorder
-}
-
-func AssertJSONSchema(t *testing.T, r io.Reader, matcher schema.Matcher) {
-	err := schema.MatchJSON(matcher, r)
-	if err != nil {
-		th.Error(t, 1, err.Error())
-	}
-}
-
-func MustMatchJSONSchema(t *testing.T, r io.Reader, matcher schema.Matcher) {
-	err := schema.MatchJSON(matcher, r)
-	if err != nil {
-		th.Error(t, 1, err.Error())
-		t.FailNow()
-	}
 }
