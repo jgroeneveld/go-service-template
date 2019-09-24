ac61836ef1e4e2f5538803a064833d15f8f01154
Jaap Groeneveld <moin@jgroeneveld.de>
2019-09-24 14:22:59 +0200

    add middleware

--- app/server.go
+++ app/server.go
@@ -3,6 +3,7 @@ package app
 import (
 	"fmt"
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"net/http"
 )
 
@@ -17,6 +18,11 @@ func StartServer(port string) error {
 
 func routes() *chi.Mux {
 	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	router.Use(middleware.RealIP)
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
 
 	router.Get("/", rootHandler)
 
